0646954039005fa0d510df0a1a0dc228bf716ca6
Alexan <hayrapetyan.alexan@gmail.com>
2024-12-10 15:35:20 +0400

    Alcray/autoform full translate (#262)
    Signed-off-by: Alexan <hayrapetyan.alexan@gmail.com>

--- nemo_skills/inference/server/model.py
+++ nemo_skills/inference/server/model.py
@@ -343,6 +343,7 @@ class TRTLLMModel(BaseModel):
 
         Not every server supports that, so make sure to override this method directly if that's not the case.
         """
+
         kwargs = {
             'tokens_to_generate': tokens_to_generate,
             'temperature': temperature,
