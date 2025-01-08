34a381cefe99059ca21f93744301b05f55b1793e
Igor Gitman <igitman@nvidia.com>
2025-01-08 11:52:32 -0800

    Update trtllm stack
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/inference/server/serve_trt.py
+++ nemo_skills/inference/server/serve_trt.py
@@ -25,14 +25,13 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
-import numpy as np
 import tensorrt_llm
 import tensorrt_llm.bindings.executor as trtllm
 import torch
 from fastapi import FastAPI, HTTPException
 from mpi4py import MPI
 from pydantic import BaseModel
-from tensorrt_llm.runtime.model_runner_cpp import ModelRunnerCpp
+from tensorrt_llm.runtime.model_runner_cpp import ExternalDraftTokensConfig, ModelRunnerCpp
 from transformers import AutoTokenizer
 
 app = FastAPI(title="TensorRT-LLM Server")
@@ -96,12 +95,18 @@ def generate(
     runner,
     batch_input_ids: List[torch.Tensor],
     *,
+    position_ids: List[torch.Tensor] = None,
     encoder_input_ids: List[torch.Tensor] = None,
+    encoder_input_features: List[torch.Tensor] = None,  # TODO: add to doc string
+    encoder_output_lengths: List[int] = None,
+    cross_attention_masks: List[torch.Tensor] = None,  # TODO: add to doc string
+    mrope_params=None,
     sampling_config=None,
     lora_uids: Optional[list] = None,
+    lookahead_config: list[int] | None = None,
     streaming: bool = False,
     stopping_criteria=None,
-    logits_processor=None,
+    logits_processor_names: list[str] | None = None,
     max_new_tokens: int = 1,
     end_id: int | None = None,
     pad_id: int | None = None,
@@ -113,10 +118,11 @@ def generate(
     output_cum_log_probs: bool = False,
     prompt_table=None,
     prompt_tasks: Optional[str] = None,
+    input_token_extra_ids: List[List[int]] = None,
     return_all_generated_tokens: bool = False,
-    input_lengths=None,
     tokenizer=None,
     timeout=None,
+    input_lengths=None,
     **kwargs,
 ):
     """
@@ -127,6 +133,14 @@ def generate(
     Args:
         batch_input_ids (List[torch.Tensor]):
             A list of input id tensors. Each tensor is of shape (sequence_length, ).
+        position_ids (List[torch.Tensor]):
+            A list of position id tensors. Each tensor is of shape (sequence_length, ).
+        encoder_input_ids (List[torch.Tensor]):
+            A list of encoder input id tensors for encoder-decoder models (optional). Each tensor is of shape (sequence_length, ).
+        encoder_input_features: (List[torch.Tensor]):
+            A list of encoder input feature tensors for multimodal encoder-decoder models (optional). Each tensor is of shape (sequence_length, feature_dim).
+        encoder_output_lengths: (List[int]):
+            A list of encoder output lengths (optional) if encoder output has different length from encoder input (due to convolution down-sampling, etc.)
         sampling_config (SamplingConfig):
             The sampling configuration to be used as base parametrization for the generation call.
             The passed **kwargs matching the sampling_config's attributes will override them.
@@ -135,14 +149,16 @@ def generate(
             The file path of prompt table (.npy format, exported by nemo_prompt_convert.py) or the prompt table itself.
         prompt_tasks (str):
             The prompt tuning task ids for the input batch, in format of comma-separated list (e.g., 0,3,1,0).
+        input_token_extra_ids (List[List[int]]):
+            Input token extra ids for using p-tuning and KV Cache reuse together
         lora_uids (list):
             The uids of LoRA weights for the input batch. Use -1 to disable the LoRA module.
         streaming (bool):
             Whether or not to use streaming mode for generation.
         stopping_criteria (StoppingCriteria):
             Custom stopping criteria.
-        logits_processor (LogitsProcessor):
-            Custom logits processors.
+        logits_processor_names (List[str]):
+            Custom logits processor names.
         return_all_generated_tokens (bool):
             Whether the full output is returned at each streaming step
         kwargs (Dict[str, Any]:
@@ -158,12 +174,11 @@ def generate(
     """
     assert streaming
     # TODO: Check if these can be supported now and support them
-    if lora_uids is not None:
-        raise RuntimeError("LoRA is not supported in C++ session.")
     if stopping_criteria is not None:
         raise RuntimeError("Stopping criteria is not supported in C++ session.")
-    if logits_processor is not None:
-        raise RuntimeError("Logits processor is not supported in C++ session.")
+
+    if not runner.use_kv_cache and max_new_tokens > 1:
+        raise RuntimeError('Disabled KV cache is intended for context phase only now.')
 
     # If we are in a multi-gpu scenario, only rank 0 continues
     if not runner.session.can_enqueue_requests():
@@ -183,9 +198,8 @@ def generate(
             "top_p_min",
             "top_p_reset_ids",
             "top_p_decay",
-            "random_seed",
             "temperature",
-            "min_length",
+            "min_tokens",
             "beam_search_diversity_rate",
             "repetition_penalty",
             "presence_penalty",
@@ -193,6 +207,8 @@ def generate(
             "length_penalty",
             "early_stopping",
             "no_repeat_ngram_size",
+            "random_seed",
+            "num_return_sequences",
         ]
         rename_params = {"num_beams": "beam_width", "random_seed": "seed"}
         sampling_params = {k: v for k, v in kwargs.items() if k in accepted_parameters}
@@ -207,12 +223,7 @@ def generate(
     else:
         sampling_config = copy.deepcopy(sampling_config)
 
-    runner._check_inputs(
-        batch_input_ids_list,
-        sampling_config=sampling_config,
-        max_new_tokens=max_new_tokens,
-        encoder_input_ids=encoder_input_ids,
-    )
+    runner._check_inputs(batch_input_ids_list, encoder_input_ids_list, sampling_config, max_new_tokens)
 
     output_config = trtllm.OutputConfig(
         return_context_logits=runner.gather_context_logits,
@@ -221,53 +232,118 @@ def generate(
     )
 
     prompt_tuning_configs = runner._prepare_ptuning_executor(
-        batch_input_ids_list, prompt_table, prompt_tasks, input_token_extra_ids=None
+        batch_input_ids_list, prompt_table, prompt_tasks, input_token_extra_ids
     )
+    mrope_configs = runner._prepare_mrope_executor(batch_input_ids_list, mrope_params)
 
-    # not letting trtllm handle stop words as this is only supported on a token-level
     stop_words_list_none = runner._prepare_words_list(None, len(batch_input_ids_list))
     bad_words_list = runner._prepare_words_list(bad_words_list, len(batch_input_ids_list))
+    logits_processor_names = runner._prepare_names_list(logits_processor_names, len(batch_input_ids_list))
+
+    lora_configs = runner._prepare_lora_configs(lora_uids, len(batch_input_ids_list))
+    request_lookahead_config = None
+    if lookahead_config is not None:
+        [w, n, g] = lookahead_config
+        request_lookahead_config = trtllm.LookaheadDecodingConfig(w, n, g)
+    skip_cross_attn_blocks = kwargs.get('skip_cross_attn_blocks', None)
+
+    # Draft-Target-Model speculative decoding
+    if (
+        "draft_tokens_list" in kwargs.keys()
+        and kwargs["draft_tokens_list"] is not None
+        and "draft_logits_list" in kwargs.keys()
+        and kwargs["draft_logits_list"] is not None
+    ):
+        # Use logits to accept
+        external_draft_tokens_configs = [
+            ExternalDraftTokensConfig(draft_tokens, draft_logits)
+            for draft_tokens, draft_logits in zip(kwargs["draft_tokens_list"], kwargs["draft_logits_list"])
+        ]
+        is_draft_target_model = True
+    elif "draft_tokens_list" in kwargs.keys() and kwargs["draft_tokens_list"] is not None:
+        # Use tokens to accept
+        external_draft_tokens_configs = [
+            ExternalDraftTokensConfig(draft_tokens) for draft_tokens in kwargs["draft_tokens_list"]
+        ]
+        is_draft_target_model = True
+    else:
+        external_draft_tokens_configs = [None] * len(batch_input_ids_list)
+        is_draft_target_model = False
 
     requests = [
         trtllm.Request(
             input_token_ids=input_ids,
             encoder_input_token_ids=encoder_input_ids_list[i] if encoder_input_ids is not None else None,
+            encoder_output_length=encoder_output_lengths[i] if encoder_output_lengths is not None else None,
+            encoder_input_features=(
+                encoder_input_features[i].contiguous() if encoder_input_features is not None else None
+            ),
+            position_ids=position_ids[i].tolist() if position_ids is not None else None,
+            cross_attention_mask=(
+                cross_attention_masks[i].contiguous()
+                if (cross_attention_masks is not None and cross_attention_masks[i] is not None)
+                else None
+            ),
             max_tokens=max_new_tokens,
             pad_id=pad_id,
             end_id=end_id,
             stop_words=stop_words,
             bad_words=bad_words,
             sampling_config=sampling_config,
+            lookahead_config=request_lookahead_config,
             streaming=streaming,
             output_config=output_config,
             prompt_tuning_config=prompt_tuning_config,
+            mrope_config=mrope_config,
+            lora_config=lora_config,
             return_all_generated_tokens=return_all_generated_tokens,
+            logits_post_processor_name=logits_post_processor_name,
+            external_draft_tokens_config=external_draft_tokens_config,
+            skip_cross_attn_blocks=skip_cross_attn_blocks,
         )
-        for i, (input_ids, stop_words, bad_words, prompt_tuning_config) in enumerate(
-            zip(batch_input_ids_list, stop_words_list_none, bad_words_list, prompt_tuning_configs)
+        for i, (
+            input_ids,
+            stop_words,
+            bad_words,
+            prompt_tuning_config,
+            mrope_config,
+            lora_config,
+            logits_post_processor_name,
+            external_draft_tokens_config,
+        ) in enumerate(
+            zip(
+                batch_input_ids_list,
+                stop_words_list_none,
+                bad_words_list,
+                prompt_tuning_configs,
+                mrope_configs,
+                lora_configs,
+                logits_processor_names,
+                external_draft_tokens_configs,
+            )
         )
     ]
 
     request_ids = runner.session.enqueue_requests(requests)
-
     return _stream(
-        runner,
-        request_ids,
-        end_id,
-        return_dict,
-        output_sequence_lengths,
-        output_log_probs,
-        output_cum_log_probs,
-        batch_input_ids,
-        streaming,
-        batch_input_ids_list,
-        return_all_generated_tokens,
-        stop_words_list,
-        tokenizer,
-        input_lengths,
+        runner=runner,
+        request_ids=request_ids,
+        end_id=end_id,
+        return_dict=return_dict,
+        output_sequence_lengths=output_sequence_lengths,
+        output_log_probs=output_log_probs,
+        output_cum_log_probs=output_cum_log_probs,
+        batch_input_ids=batch_input_ids,
+        batch_input_ids_list=batch_input_ids_list,
+        streaming=streaming,
+        return_all_generated_tokens=return_all_generated_tokens,
         max_new_tokens=max_new_tokens,
         sampling_config=sampling_config,
+        is_draft_target_model=is_draft_target_model,
+        stop_words_list=stop_words_list,
+        tokenizer=tokenizer,
         timeout=timeout,
+        input_lengths=input_lengths,
     )
 
 
@@ -287,17 +363,18 @@ def _stream(
     tokenizer,
     input_lengths,
     max_new_tokens: int,
-    num_return_sequences: int = 1,
     sampling_config=None,
     timeout=None,
+    is_draft_target_model: bool = False,
 ):
     if stop_words_list is None:
         stop_words_list = []
     assert len(request_ids) == 1
-    output_ids = [[] for _ in range(len(request_ids))]
-    for reqid_pos in range(len(request_ids)):
-        output_ids[reqid_pos] = [copy.deepcopy(batch_input_ids_list[reqid_pos]) for _ in range(runner.max_beam_width)]
-
+    num_sequences = runner._get_num_sequences(sampling_config)
+    output_ids = [
+        [copy.deepcopy(batch_input_ids_list[batch_idx]) for _ in range(num_sequences)]
+        for batch_idx in range(len(request_ids))
+    ]
     # checking the last 20 tokens for stop words
     num_tokens_to_check = 20
 
@@ -312,7 +389,6 @@ def _stream(
             if response.result.is_final:
                 finished_reqs += 1
 
-        # TODO : Handle draft target model correctly
         output = runner._fill_output(
             responses=responses,
             output_ids=output_ids,
@@ -328,7 +404,7 @@ def _stream(
             return_all_generated_tokens=return_all_generated_tokens,
             max_new_tokens=max_new_tokens,
             sampling_config=sampling_config,
-            is_draft_target_model=False,
+            is_draft_target_model=is_draft_target_model,
         )
 
         matching_stop_word = None
