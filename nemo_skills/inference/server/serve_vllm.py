40926a8d182676d28eb25116f2c2b3bab0c54120
Igor Gitman <igitman@nvidia.com>
2025-01-21 15:35:17 -0800

    Another try
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/inference/server/serve_vllm.py
+++ nemo_skills/inference/server/serve_vllm.py
@@ -14,18 +14,9 @@
 
 import argparse
 import os
-import socket
 import subprocess
 
-
-def get_slurm_info():
-    nodelist = os.environ.get('SLURM_STEP_NODELIST') or os.environ.get('SLURM_JOB_NODELIST')
-    master_addr = (
-        subprocess.check_output(f'scontrol show hostnames {nodelist} | head -n1', shell=True).decode().strip()
-    )
-    local_rank = int(os.environ.get('SLURM_LOCALID', 0))
-    world_size = int(os.environ.get('SLURM_NTASKS', 1))
-    return master_addr, local_rank, world_size
+import ray
 
 
 def main():
@@ -35,15 +26,23 @@ def main():
     parser.add_argument("--port", type=int, default=5000, help="Server port")
     args, unknown = parser.parse_known_args()
 
-    master_addr, local_rank, world_size = get_slurm_info()
-    is_master = local_rank == 0
-    dist_port = args.port + 1  # Use separate port for distributed communication
-
     extra_arguments = f'{" ".join(unknown)}'
 
-    if is_master:
-        print(f"Deploying model {args.model} across {world_size} nodes")
-        print("Starting OpenAI Server")
+    print(f"Deploying model {args.model}")
+    print("Starting OpenAI Server")
+
+    # TODO: don't break local
+    # Get node information from SLURM env vars
+    node_rank = int(os.environ["SLURM_PROCID"])
+    head_node = os.environ["SLURM_NODELIST"].split(",")[0]
+    print(f"Node rank: {node_rank}, head node: {head_node}")
+    print(f"All nodes: {os.environ['SLURM_NODELIST']}")
+
+    # Initialize Ray based on node rank
+    if node_rank == 0:
+        ray.init(_node_ip_address=head_node)
+    else:
+        ray.init(address=f"{head_node}:6379")
 
     cmd = (
         f'python -m vllm.entrypoints.openai.api_server '
@@ -52,15 +51,12 @@ def main():
         f'    --trust-remote-code '
         f'    --host="0.0.0.0" '
         f'    --port={args.port} '
-        f'    --tensor-parallel-size={args.num_gpus * world_size} '
+        f'    --tensor-parallel-size={args.num_gpus} '
         f'    --gpu-memory-utilization=0.9 '
         f'    --max-num-seqs=256 '
         f'    --enforce-eager '
         f'    --disable-log-requests '
         f'    --disable-log-stats '
-        f'    --master-addr={master_addr} '
-        f'    --master-port={dist_port} '
-        f'    --worker-use-ray '
         f'    {extra_arguments} | grep -v "200 OK"'
     )
 
