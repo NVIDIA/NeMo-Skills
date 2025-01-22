de09751bd0e9c0a658a464675e9a36cbf57eb703
Igor Gitman <igitman@nvidia.com>
2025-01-21 16:25:12 -0800

    WIP
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/inference/server/serve_vllm.py
+++ nemo_skills/inference/server/serve_vllm.py
@@ -34,7 +34,7 @@ def main():
     # TODO: don't break local
     # Get node information from SLURM env vars
     node_rank = int(os.environ["SLURM_PROCID"])
-    head_node = os.environ["SLURM_NODELIST"].split(",")[0]
+    head_node = os.environ["SLURM_NODELIST"].split(",")[0].replace("[", "").replace("]", "")
     print(f"Node rank: {node_rank}, head node: {head_node}")
     print(f"All nodes: {os.environ['SLURM_NODELIST']}")
 
