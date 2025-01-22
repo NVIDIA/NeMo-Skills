9785c826e0ae2df6d1334eaf95e6b3148c89c4b9
Somshubra Majumdar <titu1994@gmail.com>
2025-01-21 18:46:21 -0800

    Match above code
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo_skills/pipeline/utils.py
+++ nemo_skills/pipeline/utils.py
@@ -532,14 +532,13 @@ def get_git_repo_path(path: str | Path = None):
         Path to the repo if it is a git repo, otherwise None.
     """
     original_path = os.getcwd()
-    command = ["git", "rev-parse", "--show-toplevel"]
     try:
         if path:
             os.chdir(path)
 
         repo_path = (
             subprocess.run(
-                command,
+                ["git", "rev-parse", "--show-toplevel"],
                 capture_output=True,
                 check=True,
             )
@@ -561,11 +560,9 @@ def get_packager(extra_package_dirs: tuple[str] | None = None):
     if extra_package_dirs:
         include_patterns = [str(Path(d) / '*') for d in extra_package_dirs]
         include_pattern_relative_paths = [str(Path(d).parent) for d in extra_package_dirs]
-        # repo_types = ["git" if get_git_repo_path(d) else "pattern" for d in extra_package_dirs]
     else:
         include_patterns = []
         include_pattern_relative_paths = []
-        repo_types = []
 
     check_uncommited_changes = not bool(os.getenv('NEMO_SKILLS_DISABLE_UNCOMMITTED_CHANGES_CHECK', 0))
 
@@ -599,7 +596,6 @@ def get_packager(extra_package_dirs: tuple[str] | None = None):
         )
         include_patterns.append(str(nemo_skills_dir / '*'))
         include_pattern_relative_paths.append(str(nemo_skills_dir.parent))
-        # repo_types.append("git")
 
         root_package =  run.PatternPackager(
             include_pattern=include_patterns,
@@ -619,9 +615,9 @@ def get_packager(extra_package_dirs: tuple[str] | None = None):
             repo_include_pattern_relative_path = [str(Path(repo_path).parent)]
             if get_git_repo_path(repo_path):
                 extra_repos[repo_name] = run.GitArchivePackager(
-                    basepath=str(repo_path),
-                    # include_pattern=repo_include_pattern,
-                    # include_pattern_relative_path=repo_include_pattern_relative_path,
+                    # basepath=str(repo_path),
+                    include_pattern=repo_include_pattern,
+                    include_pattern_relative_path=repo_include_pattern_relative_path,
                     check_uncommitted_changes=check_uncommited_changes
                 )
             else:
