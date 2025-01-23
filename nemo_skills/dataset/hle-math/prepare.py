142792d8e8af7781f70da57475a02e962c9b805a
i-vainn <imoshkov@nvidia.com>
2025-01-23 23:43:23 +0400

    fix

--- nemo_skills/dataset/hle-math/prepare.py
+++ nemo_skills/dataset/hle-math/prepare.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
 import json
 from pathlib import Path
 from datasets import load_dataset
