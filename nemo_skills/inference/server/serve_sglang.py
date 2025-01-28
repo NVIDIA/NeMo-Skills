24996af60dce3321f2476180659797fdb792e161
Wei Du <wedu@nvidia.com>
2025-01-28 14:00:50 -0800

    test load state

--- nemo_skills/inference/server/serve_sglang.py
+++ nemo_skills/inference/server/serve_sglang.py
@@ -47,7 +47,7 @@ def main():
         f'    --tensor-parallel-size={args.num_gpus*args.num_nodes} '
         f'    --nnodes={args.num_nodes} '
         f'    --node-rank={args.node_rank} '
-        f'    --load-format="sharded_state" '
+        f'    --load-format="npcache" '
         f'    --dist-init-addr="{args.dist_init_addr}:20000" '
     )
 
