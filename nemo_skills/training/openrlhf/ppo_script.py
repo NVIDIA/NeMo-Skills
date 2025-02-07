cdf5cb9340ad4c6e06227ab3781463a40ffab9b6
George Armstrong <georgea@nvidia.com>
2025-02-06 16:08:47 -0800

    WIP copy over entier internal of ActorModelRayActor

--- nemo_skills/training/openrlhf/ppo_script.py
+++ nemo_skills/training/openrlhf/ppo_script.py
@@ -50,41 +50,185 @@ from openrlhf.trainer.ray.launcher import BasePPORole
 
 from nemo_skills.training.openrlhf.utils import MaxTimeManager
 
-from typing import Callable
+from typing import Callable, List
 import os
 
-class ActorMetaclass(type):
-    def __getattr__(cls, name):
-        if name in cls.__dict__:
-            return cls.__dict__[name]
-        return getattr(ActorModelRayActor, name)
+import itertools
+import math
+
+
+import torch
+import torch.distributed
+from transformers.trainer import get_scheduler
+
+from openrlhf.datasets import PromptDataset, SFTDataset
+from openrlhf.models import Actor
+from openrlhf.utils import blending_datasets, get_tokenizer
+from openrlhf.utils.deepspeed import DeepspeedStrategy
+
+from openrlhf.trainer.ray.ppo_actor import Actor
 
 @ray.remote(num_gpus=1)
 class CustomActorModelRayActor(BasePPORole):
-    # This is a hack to avoid having to copy all the methods in ActorModelRayActor because
-    # Ray does not allow to inherit from a remote class
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        import types
-        self.init_model_from_pretrained = types.MethodType(ActorModelRayActor.init_model_from_pretrained, self)
-        self.prepare_datasets = types.MethodType(ActorModelRayActor.prepare_datasets, self)
-        self.max_steps = types.MethodType(ActorModelRayActor.max_steps, self)
-        self.save_model = types.MethodType(ActorModelRayActor.save_model, self)
 
-    # def __init__(self, *args, **kwargs):
-        # This is a hack to avoid having to copy all the methods in ActorModelRayActor because
-        # Ray does not allow to inherit from a remote class
-        # self.base_actor = ActorModelRayActor.remote(*args, **kwargs)
+    # The only unique method here is .fit(), which is needed to override the Trainer
+    # for the ActorModelRayActor class, since it is defined as a remote class, so
+    # we cannot inherit from it directly.
 
-    # def __getattr__(self, name):
-    #     # First check if the attribute exists in this class
-    #     if name in self.__dict__:
-    #         return self.__dict__[name]
-    #     # If not, delegate to the base actor
-    #     return getattr(self.base_actor, name)
+    # method copied directly from openrlhf/trainer/ray/ppo_actor.py
+    def init_model_from_pretrained(self, strategy: DeepspeedStrategy, pretrain):
+        args = strategy.args
 
-    # this fit is copy/pasted from the original ActorModelRayActor fit method
-    # but has the trainer overridden by CustomActorPPOTrainer
+        if getattr(args, "vllm_num_engines", 0) > 0:
+            # To prevent hanging during NCCL synchronization of weights between DeepSpeed and vLLM.
+            # see https://github.com/vllm-project/vllm/blob/c6b0a7d3ba03ca414be1174e9bd86a97191b7090/vllm/worker/worker_base.py#L445
+            if getattr(args, "vllm_sync_backend", "nccl") == "nccl":
+                os.environ["NCCL_CUMEM_ENABLE"] = "0"
+
+        self._setup_distributed(strategy)
+
+        actor = Actor(
+            pretrain,
+            use_flash_attention_2=strategy.args.flash_attn,
+            bf16=strategy.args.bf16,
+            load_in_4bit=strategy.args.load_in_4bit,
+            lora_rank=strategy.args.lora_rank,
+            lora_alpha=strategy.args.lora_alpha,
+            target_modules=strategy.args.target_modules,
+            lora_dropout=strategy.args.lora_dropout,
+            ds_config=strategy.get_ds_train_config(is_actor=True),
+            packing_samples=strategy.args.packing_samples,
+        )
+        strategy.print(actor)
+
+        # configure tokenizer
+        self.tokenizer = get_tokenizer(
+            pretrain, actor.model, "left", strategy, use_fast=not strategy.args.disable_fast_tokenizer
+        )
+
+        if args.enable_ema:
+            ema_model = Actor(
+                pretrain,
+                use_flash_attention_2=strategy.args.flash_attn,
+                bf16=strategy.args.bf16,
+                load_in_4bit=strategy.args.load_in_4bit,
+                ds_config=strategy.get_ds_eval_config(offload=True),
+                packing_samples=strategy.args.packing_samples,
+            )
+        else:
+            ema_model = None
+
+        # configure optimizer
+        actor_optim = strategy.create_optimizer(
+            actor, lr=args.actor_learning_rate, betas=strategy.args.adam_betas, weight_decay=args.l2
+        )
+
+        # prepare_datasets
+        self.prepare_datasets()
+
+        # configure scheduler
+        self.num_update_steps_per_episodes = (
+            len(self.prompts_dataset) * args.n_samples_per_prompt // args.train_batch_size * args.max_epochs
+        )
+        max_steps = math.ceil(args.num_episodes * self.num_update_steps_per_episodes)
+        self._max_steps = max_steps
+
+        actor_scheduler = get_scheduler(
+            "cosine_with_min_lr",
+            actor_optim,
+            num_warmup_steps=math.ceil(max_steps * args.lr_warmup_ratio),
+            num_training_steps=max_steps,
+            scheduler_specific_kwargs={"min_lr": args.actor_learning_rate * 0.1},
+        )
+
+        if args.gradient_checkpointing:
+            actor.gradient_checkpointing_enable(
+                gradient_checkpointing_kwargs={"use_reentrant": args.gradient_checkpointing_use_reentrant}
+            )
+
+        # prepare models/optimizers...
+        self.actor, self.actor_optim, self.actor_scheduler = strategy.prepare(
+            (actor, actor_optim, actor_scheduler),
+            is_rlhf=True,
+        )
+
+        if ema_model:
+            ema_model._offload = True
+            self.ema_model = strategy.prepare(ema_model, is_rlhf=True)
+        else:
+            self.ema_model = None
+
+        # load checkpoint
+        self.consumed_samples = 0
+        ckpt_path = os.path.join(args.ckpt_path, "_actor")
+        if args.load_checkpoint and os.path.exists(ckpt_path):
+            _, states = strategy.load_ckpt(self.actor.model, ckpt_path)
+            self.consumed_samples = states["consumed_samples"]
+            strategy.print(f"Loaded the checkpoint: {ckpt_path}, consumed_samples: {self.consumed_samples}")
+
+    def prepare_datasets(self):
+        strategy = self.strategy
+        args = self.strategy.args
+
+        # prepare datasets
+        prompts_data = blending_datasets(
+            args.prompt_data,
+            args.prompt_data_probs,
+            strategy,
+            args.seed,
+            max_count=args.max_samples,
+            return_eval=False,
+            train_split=args.prompt_split,
+        )
+        prompts_data = prompts_data.select(range(min(args.max_samples, len(prompts_data))))
+        self.prompts_dataset = PromptDataset(
+            prompts_data, self.tokenizer, strategy, input_template=args.input_template
+        )
+        self.prompts_dataloader = strategy.setup_dataloader(
+            self.prompts_dataset, args.rollout_batch_size // strategy.world_size, True, True
+        )
+
+        if args.pretrain_data:
+            pretrain_data = blending_datasets(
+                args.pretrain_data,
+                args.pretrain_data_probs,
+                strategy,
+                args.seed,
+                return_eval=False,
+                train_split=args.pretrain_split,
+            )
+            pretrain_max_len = args.max_len if args.max_len else args.prompt_max_len + args.generate_max_len
+            pretrain_dataset = SFTDataset(
+                pretrain_data.select(
+                    range(
+                        min(
+                            len(pretrain_data), args.max_epochs * len(self.prompts_dataset) * args.n_samples_per_prompt
+                        )
+                    )
+                ),
+                self.tokenizer,
+                pretrain_max_len,
+                strategy,
+                pretrain_mode=True,
+            )
+            self.pretrain_dataloader = itertools.cycle(
+                iter(
+                    strategy.setup_dataloader(
+                        pretrain_dataset,
+                        args.micro_train_batch_size,
+                        True,
+                        True,
+                        pretrain_dataset.collate_fn,
+                    )
+                )
+            )
+        else:
+            self.pretrain_dataloader = None
+
+    def max_steps(self):
+        """Return the maximum number of steps."""
+        return self._max_steps
+
     def fit(self,
         critic_model: ray.actor.ActorHandle,
         initial_model: ray.actor.ActorHandle,
@@ -154,6 +298,16 @@ class CustomActorModelRayActor(BasePPORole):
             self.pretrain_dataloader,
             self.consumed_samples,
             self.num_update_steps_per_episodes,
+        )
+
+    def save_model(self):
+        args = self.strategy.args
+
+        # save model checkpoint after fitting on only rank0
+        self.strategy.save_model(
+            self.ema_model if args.enable_ema else self.actor,
+            self.tokenizer,
+            args.save_path,
         )
 
 class CustomActorPPOTrainer(ActorPPOTrainer):
