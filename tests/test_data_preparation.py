b77135fecbc24d978b4bc1eab41a9798a06581d0
Shubham Toshniwal <stoshniwal@nvidia.com>
2025-02-11 20:19:35 -0800

    Test prep

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -176,17 +176,28 @@ def test_openmathinstruct2():
 
 
 def test_aggregate_answers():
-    output_dir = f"/tmp/nemo-skills-tests/test_majority_filling"
-    docker_rm_and_mkdir(output_dir)
+    output_dir = "/tmp/nemo-skills-tests/test_majority_filling"
+    # docker_rm_and_mkdir(output_dir)
     run_cmd(
         cluster='test-local',
         config_dir=Path(__file__).parent / 'gpu-tests',
         log_dir='/tmp/nemo-skills-tests/test_aggregate_answers',
         ctx=wrap_arguments(
             f"python -m nemo_skills.evaluation.aggregate_answers "
-            f"    ++input_dir='tests/data/' "
+            f"    ++input_dir='tests/data' "
             f"    ++input_files='output-rs*.test' "
             f"    ++mode=fill "
             f"    ++output_dir={output_dir} "
         ),
     )
+
+    # Check md5 of one of the output files
+    output_file = f"{output_dir}/output-rs0.test"
+    expected_md5 = "c27bfd72287c45ad7e1fd9cd7e5cc159"
+    output_md5 = compute_md5(output_file)
+
+    print(f"output_md5: {output_md5}")
+
+    assert (
+        expected_md5 == output_md5
+    ), "MD5 hashes do not match, something is wrong with nemo_skills/evaluation/aggregate_answers.py"
