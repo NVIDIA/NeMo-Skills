b3e2fe2b93a8e5bc1449a32d4639cf1d549711d1
Igor Gitman <igitman@nvidia.com>
2025-04-22 22:49:26 -0700

    Roll-back tmp change
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/evaluation/metrics/compute_metrics.py
+++ nemo_skills/evaluation/metrics/compute_metrics.py
@@ -58,8 +58,6 @@ class ComputeMetrics:
                 data = read_predictions(predictions, idx, file_handles)
                 # checking if we need to create a new metrics calculator
                 data_subset = data[0].get('subset_for_metrics', 'all')
-                if 'hmmt' in data_subset:
-                    data_subset = 'hmmt-24-25'
                 if data_subset not in self.calculators:
                     self.calculators[data_subset] = self.get_metrics_calculator(
                         self.benchmark,
