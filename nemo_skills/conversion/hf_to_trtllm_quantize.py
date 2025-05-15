0e22d25c56f4171d6e9086324d845d49a02188d6
darraghdog <darragh.hanley@gmail.com>
2025-05-15 15:47:16 +0100

    Licence and source

--- nemo_skills/conversion/hf_to_trtllm_quantize.py
+++ nemo_skills/conversion/hf_to_trtllm_quantize.py
@@ -1,3 +1,22 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copied from NVIDIA's TensorRT-LLM project:
+# https://github.com/NVIDIA/TensorRT-LLM/blob/main/examples/quantization/quantize.py
+# Original commit: 5aec7af45fc0abd876fa68a9ae8c8cae084f3af3
+# Original version: v0.18
+
 import argparse
 
 import torch.multiprocessing as mp
