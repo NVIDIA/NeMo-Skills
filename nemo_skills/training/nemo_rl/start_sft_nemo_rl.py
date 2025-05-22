908e95d6843887fab2514116373949962f93e30b
George Armstrong <georgea@nvidia.com>
2025-05-22 16:32:46 -0700

    add copy note for sft training script

--- nemo_skills/training/nemo_rl/start_sft_nemo_rl.py
+++ nemo_skills/training/nemo_rl/start_sft_nemo_rl.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# copied from https://github.com/NVIDIA/NeMo-RL/blob/main/examples/run_sft.py
+
 import argparse
 import os
 import pprint
