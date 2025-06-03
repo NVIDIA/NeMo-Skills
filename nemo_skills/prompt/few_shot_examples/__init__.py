1173324bcdaa86c6a74a5be93cb6166e37684714
jubick1337 <mattyson.so@gmail.com>
2025-06-03 06:32:39 -0700

    Adding OpenScience prompts and scripts
    Signed-off-by: jubick1337 <mattyson.so@gmail.com>

--- nemo_skills/prompt/few_shot_examples/__init__.py
+++ nemo_skills/prompt/few_shot_examples/__init__.py
@@ -15,11 +15,13 @@ from nemo_skills.prompt.few_shot_examples.gsm8k import examples_map as examples_
 from nemo_skills.prompt.few_shot_examples.math import examples_map as examples_math
 from nemo_skills.prompt.few_shot_examples.lean4 import examples_map as examples_lean4
 from nemo_skills.prompt.few_shot_examples.mmlu_pro import examples_map as examples_mmlu_pro
+from nemo_skills.prompt.few_shot_examples.open_science import examples_map as examples_open_science
 
 examples_map = examples_gsm8k.copy()
 examples_map.update(examples_math)
 examples_map.update(examples_lean4)
 examples_map.update(examples_mmlu_pro)
+examples_map.update(examples_open_science)
 assert len(examples_map) == len(examples_gsm8k) + len(examples_math) + len(
     examples_lean4
-) + len(examples_mmlu_pro), "Duplicate keys in examples!"
+) + len(examples_mmlu_pro) + len(examples_open_science), "Duplicate keys in examples!"
