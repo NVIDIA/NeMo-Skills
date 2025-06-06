20b0620f58de01b1f5c080850b784ac92d1b8dc2
Igor Gitman <igitman@nvidia.com>
2025-06-06 15:02:10 -0700

    Finish ruler prepare.py
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/dataset/ruler/prepare.py
+++ nemo_skills/dataset/ruler/prepare.py
@@ -17,9 +17,52 @@
 
 import argparse
 import concurrent.futures
+import json
 import subprocess
 import tempfile
 from pathlib import Path
+
+# Define the configuration as a dictionary
+DEFAULT_SETTINGS = """
+PROMPT_CONFIG = "generic/default"
+DATASET_GROUP = "long-context"
+METRICS_TYPE = "ruler"
+EVAL_ARGS = "--eval_type=ruler --eval_config.match_type={match_type}"
+GENERATION_ARGS = (
+    "--inference.tokens_to_generate={tokens_to_generate} "
+    "--prefix_generation_to_response=True "
+    "--continue_prefix_generation=True"
+)
+"""
+TOKENS_TO_GENERATE = {'niah': 128, 'vt': 30, 'cwe': 120, 'fwe': 50, 'qa': 32}
+MATCH_TYPE = {'niah': 'all', 'vt': 'all', 'cwe': 'all', 'fwe': 'all', 'qa': 'part'}
+
+
+def prepare_task_for_ns(task, data_dir):
+    """Resaving from data_dir/task/test.jsonl into current folder/task/test.jsonl and adding proper init.py"""
+    original_path = Path(data_dir) / task / "test.jsonl"
+    new_path = Path(__file__).parent / task / "test.jsonl"
+    Path(new_path).parent.mkdir(parents=True, exist_ok=True)
+    with open(original_path, "r", encoding="utf-8") as fin, open(new_path, "w", encoding="utf-8") as fout:
+        for line in fin:
+            original_entry = json.loads(line)
+            new_entry = {
+                "index": original_entry["index"],
+                "question": original_entry["input"],
+                "expected_answer": original_entry["outputs"],
+                "length": original_entry["length"],
+                "generation": original_entry['answer_prefix'],
+            }
+            fout.write(json.dumps(new_entry) + "\n")
+
+    with open(new_path.parent / "__init__.py", "w", encoding="utf-8") as init_file:
+        short_name = task.split('_')[0]
+        init_file.write(
+            DEFAULT_SETTINGS.format(
+                match_type=MATCH_TYPE[short_name],
+                tokens_to_generate=TOKENS_TO_GENERATE[short_name],
+            )
+        )
 
 
 def get_ruler_data(tasks, ruler_prepare_args, tmp_data_dir=None):
@@ -47,7 +90,7 @@ def get_ruler_data(tasks, ruler_prepare_args, tmp_data_dir=None):
         files_exist = all((json_dir / fname).exists() for fname in required_files)
         if not files_exist:
             subprocess.run(
-                "git clone --branch igitman/add-write-manifest https://github.com/Kipok/RULER && "
+                "git clone https://github.com/NVIDIA/RULER && "
                 "cd RULER/scripts/data/synthetic/json && "
                 "python download_paulgraham_essay.py && bash download_qa_dataset.sh",
                 check=True,
@@ -58,7 +101,7 @@ def get_ruler_data(tasks, ruler_prepare_args, tmp_data_dir=None):
         # preparing the datasets based on user options, in parallel
         def prepare_task(task):
             subprocess.run(
-                f"python prepare.py --save_dir {Path(__file__).parent}/ruler_data --benchmark synthetic "
+                f"python prepare.py --save_dir {tmpdirname}/ruler_data --benchmark synthetic "
                 f"    --subset test --task {task} --tokenizer_type hf --model_template_type base --prepare_for_ns "
                 f"    --num_samples 500 {ruler_prepare_args}",
                 shell=True,
@@ -71,14 +114,18 @@ def get_ruler_data(tasks, ruler_prepare_args, tmp_data_dir=None):
             for future in concurrent.futures.as_completed(futures):
                 future.result()  # Will raise exception if any subprocess fails
 
+        # resaving the data and creating __init__.py files
+        for task in tasks:
+            prepare_task_for_ns(task, Path(tmpdirname) / "ruler_data")
+
     finally:
         if tmpdir_context is not None:
             tmpdir_context.__exit__(None, None, None)
 
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(description="Prepare RULER dataset.")
-    argparser.add_argument(
+    parser = argparse.ArgumentParser(description="Prepare RULER dataset.")
+    parser.add_argument(
         "--tasks",
         type=str,
         nargs="+",
@@ -99,17 +146,23 @@ if __name__ == "__main__":
         ],
         help="List of tasks to prepare for RULER dataset.",
     )
-    argparser.add_argument(
+    parser.add_argument(
         "--tmp_data_dir",
         type=str,
         default=None,
         help="Directory to store intermediate data. If not provided, a temporary directory will be created.",
     )
 
-    args, unknown = argparser.parse_known_args()
+    args, unknown = parser.parse_known_args()
     ruler_prepare_args = " ".join(unknown)
     if not ruler_prepare_args:
-        print("ERROR: Can't prepare ruler without arguments provided! Skipping the preparation step.")
+        print(
+            "ERROR: Can't prepare ruler without arguments provided! "
+            "Skipping the preparation step.\n"
+            "Example ruler prepare command:\n"
+            "ns prepare_data ruler --tokenizer_path meta-llama/Llama-3.1-8B-Instruct "
+            "--max_seq_length 131072 --num_samples 500"
+        )
         exit(0)
     print(f"Preparing RULER dataset for tasks: {args.tasks} with additional arguments: {ruler_prepare_args}")
     get_ruler_data(args.tasks, ruler_prepare_args, tmp_data_dir=args.tmp_data_dir)
