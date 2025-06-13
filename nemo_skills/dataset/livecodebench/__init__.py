e0f8997aed96852a78814ce1da3b74156d3d2065
wasiahmad <wasiahmad@ucla.edu>
2025-06-12 22:02:35 -0700

    removing imports

--- nemo_skills/dataset/livecodebench/__init__.py
+++ nemo_skills/dataset/livecodebench/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_skills.evaluation.metrics.code_metrics import LiveCodeBenchMetrics
-
 # settings that define how evaluation should be done by default (all can be changed from cmdline)
 PROMPT_CONFIG = 'eval/livecodebench/python_codegen'
 DATASET_GROUP = 'code'
