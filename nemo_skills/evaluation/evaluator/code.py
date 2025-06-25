80f16277ffebef426b50a0a87407263c2ed0d7f0
Igor Gitman <igitman@nvidia.com>
2025-06-25 15:24:48 -0700

    Changes
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/evaluation/evaluator/code.py
+++ nemo_skills/evaluation/evaluator/code.py
@@ -15,6 +15,7 @@
 
 import json
 import logging
+import re
 import shutil
 import subprocess
 import sys
@@ -22,10 +23,59 @@ from argparse import Namespace
 
 from omegaconf import OmegaConf
 
-from nemo_skills.evaluation.code_utils import preprocess_code
 from nemo_skills.utils import get_logger_name, nested_dataclass, unroll_files
 
 LOG = logging.getLogger(get_logger_name(__file__))
+
+
+def preprocess_code(generation_dict: dict, language="python"):
+    completion = generation_dict['generation']
+    completion = completion.strip()
+    completion = completion.replace("\r", "")
+
+    ##### To handle code generation by reasoning models
+    # check for <think> and </think> tags
+    if "<think>" in completion:
+        if "</think>" in completion:
+            # thinking trace completed, solution in after the trace
+            match = re.search(r"</think>\s*(.*)", completion, re.DOTALL)
+            completion = match.group(1).strip() if match else None
+        else:
+            completion = None
+
+    if completion is None:
+        generation_dict["completion"] = ""  # no valid solution generated
+        return generation_dict
+    #####
+
+    start_with_lang_tag = f'```{language}'
+    generic_start_end_tag = f'```'
+
+    if start_with_lang_tag in completion:
+        def_line = completion.index(start_with_lang_tag) + len(start_with_lang_tag)
+        completion = completion[def_line:].strip()
+        try:
+            next_line = completion.index(generic_start_end_tag)
+            completion = completion[:next_line].strip()
+        except:
+            print(completion)
+            print("================\n")
+
+    elif generic_start_end_tag in completion:
+        def_line = completion.index(generic_start_end_tag) + len(generic_start_end_tag)
+        completion = completion[def_line:].strip()
+        try:
+            next_line = completion.index(generic_start_end_tag)
+            completion = completion[:next_line].strip()
+        except:
+            print(completion)
+            print("================\n")
+
+    if completion.startswith(" "):
+        completion = completion.strip()
+
+    generation_dict["completion"] = completion
+    return generation_dict
 
 
 def install_from_git(git_url):
