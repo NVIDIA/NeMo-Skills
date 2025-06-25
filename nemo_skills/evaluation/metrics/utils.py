227d0015c2012b49fb5ad68640e6f0f23e17250f
Wei Du <wedu@nvidia.com>
2025-06-25 14:39:34 -0700

    add summarize results support for varying length

--- nemo_skills/evaluation/metrics/utils.py
+++ nemo_skills/evaluation/metrics/utils.py
@@ -28,7 +28,6 @@ def read_predictions(predictions, line_idx, file_handles, sequence_length):
             if sequence_length != None:
                 if int(prediction_dict['num_generated_tokens']) > sequence_length: 
                     prediction_dict['is_correct'] = False
-                
         except Exception as e:
             LOG.error(f"\n\n ***** Error reading line %s in file %s: %s", line_idx + 1, file_handles[file_idx].name, e)
             raise
