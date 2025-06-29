9f93a8b94c50d562947d838b0a4f22cfdc565d8e
Evelina <ebakhturina@nvidia.com>
2025-06-29 10:30:22 -0700

    fix mcq
    Signed-off-by: Evelina <ebakhturina@nvidia.com>

--- nemo_skills/evaluation/evaluator/mcq.py
+++ nemo_skills/evaluation/evaluator/mcq.py
@@ -24,11 +24,40 @@ LOG = logging.getLogger(get_logger_name(__file__))
 
 
 def eval_mcq(cfg):
+    def extract_letter(text):
+        # extract prediction from boxed{}
+        parsed = extract_answer(text)
+        if parsed is not None and len(parsed) != 1:
+            match = re.findall(r"\b[A-J]\b(?!.*\b[A-J]\b)", parsed, re.DOTALL)
+            if len(match) > 0:
+                parsed = match[-1].strip()
+
+        if parsed is not None:
+            return parsed
+
+        def format_func(s):
+            return (
+                s.replace("(", "")
+                .replace(")", "")
+                .replace(".", "")
+                .replace(":", "")
+                .replace("*", "")
+                .replace("{", "")
+                .replace("}", "")
+                .strip()
+            )
+
+        paren_pattern = r"[\(\s*]\w+[\).:*]?"
+        paren_matches = [format_func(p) for p in re.findall(paren_pattern, text)]
+        paren_matches = [p for p in paren_matches if p in "ABCDEFGHIJ"]
+        return paren_matches[-1] if paren_matches else None
+
+
     for file in unroll_files(cfg.input_files):
         with open(file, 'rt', encoding='utf-8') as fin:
             data = [json.loads(line) for line in fin]
         with open(file, 'wt', encoding='utf-8') as fout:
             for sample in tqdm(data):
-                sample['predicted_answer'] = extract_answer(sample["generation"])
+                sample['predicted_answer'] = extract_letter(sample["generation"])
                 sample['is_correct'] = sample['predicted_answer'] == sample['expected_answer']
                 fout.write(json.dumps(sample) + "\n")
