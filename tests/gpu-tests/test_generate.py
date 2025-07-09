d112e661553307725bb1f20133e1449a8a6c9b79
Igor Gitman <igitman@nvidia.com>
2025-07-09 12:17:19 -0700

    Fix test
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/gpu-tests/test_generate.py
+++ tests/gpu-tests/test_generate.py
@@ -19,6 +19,7 @@ from pathlib import Path
 
 import pytest
 
+from nemo_skills.evaluation.metrics import ComputeMetrics
 from tests.conftest import docker_rm
 
 # TODO: retrieval test
@@ -150,9 +151,10 @@ def test_vllm_generate_seeds():
             assert 'generation' in data
         assert os.path.exists(f"{output_dir}/output-rs{seed}.jsonl.done")
 
-    with open(f"{output_dir}/eval-results/gsm8k/metrics.json", 'r') as f:
-        metrics = json.load(f)["gsm8k"]["majority@3"]
-
+    # running compute_metrics to check that results are expected
+    metrics = ComputeMetrics(benchmark='gsm8k').compute_metrics([f"{output_dir}/output-rs*.jsonl"])["all"][
+        "majority@3"
+    ]
     # rough check, since exact accuracy varies depending on gpu type
     assert metrics['symbolic_correct'] >= 50
     assert metrics['num_entries'] == 10
