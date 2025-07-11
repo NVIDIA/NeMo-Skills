e30aedffaa72c3c1edc0de5d520f7c18ab754da9
smajumdar <titu1994@gmail.com>
2025-07-10 22:09:31 -0700

    Refactor prepare_questions.py to improve output directory handling and add experiment name argument

--- recipes/opencodereasoning/pipeline/prepare_questions.py
+++ recipes/opencodereasoning/pipeline/prepare_questions.py
@@ -16,9 +16,8 @@ import argparse
 import os
 from pathlib import Path
 
-from omegaconf import OmegaConf
-
 from nemo_skills.pipeline.cli import run_cmd, wrap_arguments
+from nemo_skills.pipeline.utils import get_cluster_config, resolve_mount_paths
 
 
 def prepare_questions(cluster, expname, output_dir: str):
@@ -29,7 +28,11 @@ def prepare_questions(cluster, expname, output_dir: str):
         output_dir (str): Directory to save the prepared questions.
     """
     command = ("python /nemo_run/code/recipes/opencodereasoning/scripts/prepare_questions.py "
-               f"--output_dir '{output_dir}'")
+               "--output_dir /results")
+
+    mount_paths = f'{output_dir}:/results'
+    cluster = get_cluster_config(cluster)
+    output_dir = resolve_mount_paths(cluster, mount_paths, create_remote_dir=True)
 
     run_cmd(
         ctx=wrap_arguments(command),
@@ -46,6 +49,9 @@ if __name__ == "__main__":
     parser.add_argument('--cluster', type=str, required=True,
                         help="Cluster name to run the job on.")
 
+    parser.add_argument('--expname', type=str, required=True,
+                        help="Experiment name for the job.")
+
     parser.add_argument(
         "--output_dir",
         type=str,
@@ -60,6 +66,6 @@ if __name__ == "__main__":
 
     prepare_questions(
         cluster=args.cluster,
-        expname="prepare_questions",
+        expname=f"{args.exp_name}-prepare-questions",
         output_dir=args.output_dir,
     )
