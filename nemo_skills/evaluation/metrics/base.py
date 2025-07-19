052a8c7a32a208ee0a954015a2be647f0cc5313d
Igor Gitman <igitman@nvidia.com>
2025-07-18 17:13:23 -0700

    Fix a typo
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/evaluation/metrics/base.py
+++ nemo_skills/evaluation/metrics/base.py
@@ -23,24 +23,23 @@ class BaseMetrics(abc.ABC):
         self.reset()
 
     def update_common_metrics(self, agg_dict):
-        agg_dict = {"num_entries": self.total}
+        agg_dict["num_entries"] = self.total
         if self.avg_tokens > 0:
             agg_dict['avg_tokens'] = int(self.avg_tokens / self.total)
-        if self.avg_generation_time > 0:
-            agg_dict['total_generation_seconds'] = int(self.max_end_time - self.min_start_time)
-        if self.avg_tokens > 0 and self.avg_generation_time > 0:
-            agg_dict['avg_tokens_per_second'] = int(self.avg_tokens / self.avg_generation_time)
+        if self.max_end_time > float('-inf') and self.min_start_time < float('inf'):
+            agg_dict['gen_seconds'] = int(self.max_end_time - self.min_start_time)
 
     def get_metrics(self):
         metrics_dict = {}
         for agg_mode, agg_metric_dict in self.eval_dict.items():
             for metric_key, metric_value in agg_metric_dict.items():
-                self.update_common_metrics(metrics_dict[agg_mode])
+                metrics_dict[agg_mode] = {}
                 if isinstance(metric_value, float):
                     # by default we will return all float metrics as percentages
                     metrics_dict[agg_mode][metric_key] = 100.0 * metric_value / self.total
                 else:
                     metrics_dict[agg_mode][metric_key] = metric_value
+                self.update_common_metrics(metrics_dict[agg_mode])
 
         return metrics_dict
 
@@ -77,9 +76,6 @@ class BaseMetrics(abc.ABC):
         self.avg_tokens += sum(
             pred['num_generated_tokens'] for pred in predictions if 'num_generated_tokens' in pred
         ) / len(predictions)
-        self.avg_generation_time += sum(
-            pred['generation_time'] for pred in predictions if 'generation_time' in pred
-        ) / len(predictions)
         self.min_start_time = min(
             self.min_start_time,
             min(pred['generation_start_time'] for pred in predictions if 'generation_start_time' in pred),
@@ -93,7 +89,6 @@ class BaseMetrics(abc.ABC):
         self.total = 0
         self.max_k = 0
         self.avg_tokens = 0
-        self.avg_generation_time = 0
         self.min_start_time = float('inf')
         self.max_end_time = float('-inf')
         self.eval_dict = defaultdict(lambda: defaultdict(float))
