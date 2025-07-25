902060513c6626ccea9833daa94f2faeb376a27e
Igor Gitman <igitman@nvidia.com>
2025-07-25 10:58:51 -0700

    Add swe-bench dataset
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

---
+++ nemo_skills/dataset/swe-bench/prepare.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import shutil
+from pathlib import Path
+
+import datasets
+
+if __name__ == "__main__":
+    # TODO: support these with options
+    dataset = "princeton-nlp/SWE-bench_Verified"
+    split = "test"
+    dataset = datasets.load_dataset(path=dataset, split=split)
+    output_file = Path(__file__).parent / "test.jsonl"
+    # Convert the dataset to JSONL format
+    dataset.to_json(output_file, orient="records", lines=True)
