b35e2194e04bc257d147dcb9869d8a5c58601856
Igor Gitman <igitman@nvidia.com>
2025-08-07 15:03:06 -0700

    Hacky completions + new prompt
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/inference/generate.py
+++ nemo_skills/inference/generate.py
@@ -25,6 +25,18 @@ from typing import Any
 
 import hydra
 from omegaconf import ListConfig, OmegaConf, open_dict
+from openai_harmony import (
+    Author,
+    Conversation,
+    DeveloperContent,
+    HarmonyEncodingName,
+    Message,
+    ReasoningEffort,
+    Role,
+    SystemContent,
+    ToolDescription,
+    load_harmony_encoding,
+)
 from tqdm import tqdm
 
 from nemo_skills.code_execution.sandbox import get_sandbox, sandbox_params
@@ -382,6 +394,72 @@ class GenerationTask:
                 filled_prompt[-1]['content'] += self.cfg.prompt_suffix
             else:
                 filled_prompt += self.cfg.prompt_suffix
+
+        encoding = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
+
+        system_message = (
+            SystemContent.new()
+            .with_model_identity("You are ChatGPT, a large language model trained by OpenAI.")
+            .with_reasoning_effort(ReasoningEffort.HIGH)
+            .with_conversation_start_date("2025-06-28")
+            .with_knowledge_cutoff("2024-06")
+            .with_required_channels(["analysis", "commentary", "final"])
+        )
+
+        developer_message = DeveloperContent.new()
+        #        .with_instructions("Always respond in riddles")
+        #        .with_function_tools(
+        #            [
+        #                ToolDescription.new(
+        #                    "get_location",
+        #                    "Gets the location of the user.",
+        #                ),
+        #                ToolDescription.new(
+        #                    "get_current_weather",
+        #                    "Gets the current weather in the provided location.",
+        #                    parameters={
+        #                        "type": "object",
+        #                        "properties": {
+        #                            "location": {
+        #                                "type": "string",
+        #                                "description": "The city and state, e.g. San Francisco, CA",
+        #                            },
+        #                            "format": {
+        #                                "type": "string",
+        #                                "enum": ["celsius", "fahrenheit"],
+        #                                "default": "celsius",
+        #                            },
+        #                        },
+        #                        "required": ["location"],
+        #                    },
+        #                ),
+        #            ]
+        #        )
+        # )
+
+        convo = Conversation.from_messages(
+            [
+                Message.from_role_and_content(Role.SYSTEM, system_message),
+                Message.from_role_and_content(Role.DEVELOPER, developer_message),
+                Message.from_role_and_content(Role.USER, filled_prompt[-1]['content']),
+                # Message.from_role_and_content(
+                #     Role.ASSISTANT,
+                #     'User asks: "What is the weather in Tokyo?" We need to use get_weather tool.',
+                # ).with_channel("analysis"),
+                # Message.from_role_and_content(Role.ASSISTANT, '{"location": "Tokyo"}')
+                # .with_channel("commentary")
+                # .with_recipient("functions.get_weather")
+                # .with_content_type("json"),
+                # Message.from_author_and_content(
+                #     Author.new(Role.TOOL, "functions.lookup_weather"),
+                #     '{ "temperature": 20, "sunny": true }',
+                # ).with_recipient("assistant").with_channel("commentary"),
+            ]
+        )
+
+        tokens = encoding.render_conversation_for_completion(convo, Role.ASSISTANT)
+        filled_prompt = encoding.decode(tokens)
+
         return filled_prompt
 
     def dump_outputs(self, outputs, data_points, fout):
