3557d0255c680add4b62148e98f85c48a352b801
Shubham Toshniwal <stoshniwal@nvidia.com>
2025-08-15 22:03:37 -0700

    Removing comments
    Signed-off-by: Shubham Toshniwal <stoshniwal@nvidia.com>

--- nemo_skills/evaluation/metrics/base.py
+++ nemo_skills/evaluation/metrics/base.py
@@ -176,8 +176,6 @@ class BaseMetrics(abc.ABC):
                     majority_score = 0
                     majority_answer = None
                 else:
-                    # # sorting to ensure reproducible scores in case of ties in majority
-                    # valid_answers_and_results = sorted(valid_answers_and_results)
                     # Find the most common answer and its correctness
                     majority_count = Counter(valid_answers_and_results).most_common(1)[0][1]
                     majority_answer_list = [
