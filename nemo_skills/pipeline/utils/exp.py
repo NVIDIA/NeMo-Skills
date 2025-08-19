a8e8e85d544253d9aa91928c545db52f90f77e10
Ubuntu <ubuntu@crusoe-prod--inst-318xaf3oneqz9es4es9herzbtyh.us-east1-a.compute.internal>
2025-08-19 20:16:53 +0000

    updates

--- nemo_skills/pipeline/utils/exp.py
+++ nemo_skills/pipeline/utils/exp.py
@@ -498,7 +498,7 @@ def add_task(
                 gpus_per_node=0,
                 partition=partition,
                 time_min=time_min,
-                mounts=[],  # we don't want to mount anything
+                mounts=["/home/ubuntu/pi-coder/pi-coder-nemoskill/nemo_skills/dataset:/nemo_run/code/nemo_skills/dataset"],  # we don't want to mount anything
                 dependencies=dependencies,
                 job_name=task_name,
                 log_dir=log_dir,
