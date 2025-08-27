f6fb35396510c6070cc034cc1f5093e22ca04b42
Shubham Toshniwal <stoshniwal@nvidia.com>
2025-08-27 11:18:13 -0700

    Debugging
    Signed-off-by: Shubham Toshniwal <stoshniwal@nvidia.com>

--- nemo_skills/prompt/utils.py
+++ nemo_skills/prompt/utils.py
@@ -267,6 +267,8 @@ class Prompt:
                 "format, so please set tokenizer to a valid value."
             )
 
+        LOG.info(f"Messages: {str(messages)[:100]}")
+
         if self.tokenizer is not None:
             chat_template_kwargs = chat_template_kwargs or {}
             try:
