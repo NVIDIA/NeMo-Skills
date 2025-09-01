61cb66e8cf6a4516bf0baf4453ee3d0f4e46926f
Grigor Nalbandyan <gnalbandyan@nvidia.com>
2025-09-01 09:31:41 -0700

    Fix CR place in metrics

--- nemo_skills/pipeline/summarize_robustness.py
+++ nemo_skills/pipeline/summarize_robustness.py
@@ -370,8 +370,7 @@ def summarize_robustness(
         metric_ranges = calculate_metric_range(input_files)
         consistency_rate = calculate_consistency_rate(input_files)
         rob_metrics_to_print[benchmark] = metric_ranges
-        rob_metrics_to_print[benchmark]['cons_rate'] = consistency_rate
-    print(rob_metrics_to_print)
+        rob_metrics_to_print[benchmark]['symbolic_correct']['cons_rate'] = consistency_rate
     # grouping benchmarks that have a "." e.g ruler.niah_single_1, ruler.niah_single_2 -> ruler
     # to report average numbers
     add_benchmark_groups(results, metrics_to_print, evaluations_to_print)
@@ -383,9 +382,7 @@ def summarize_robustness(
     print("-" * 80)
     # Print all rows
     for benchmark, metrics in rob_metrics_to_print.items():
-        print(benchmark, metrics)    
         for metric_name, values in metrics.items():
-            print(metric_name, values)
             header = f"{benchmark:<15} | {metric_name:<20} |"
             header += '|'.join(f" {agg_val:<8}" for agg_fn, agg_val in values.items())
             print(header)
