45d98e97556d229d201a76120dbe23e5cc112555
fayejf <fayejf07@gmail.com>
2025-09-22 11:33:25 -0700

    temp test judge
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- nemo_skills/dataset/aalcr/__init__.py
+++ nemo_skills/dataset/aalcr/__init__.py
@@ -19,17 +19,17 @@ METRICS_TYPE = "aalcr"
 EVAL_ARGS = "++eval_type=no-op"  # using judgement directly in metrics, no need for special evaluation
 GENERATION_ARGS = ""
 
-JUDGE_PIPELINE_ARGS = {
-    "model": "gpt-4.1",
-    "server_type": "openai",
-    "server_address": "https://api.openai.com/v1",
-}
+# JUDGE_PIPELINE_ARGS = {
+#     "model": "gpt-4.1",
+#     "server_type": "openai",
+#     "server_address": "https://api.openai.com/v1",
+# }
 
 JUDGE_ARGS = "++prompt_config=judge/aalcr ++generation_key=judgement ++add_generation_stats=False"
 
 # AA-LCR official judge model.
-# JUDGE_PIPELINE_ARGS = {
-#     "model": "/hf_models/Qwen3-235B-A22B-Instruct-2507",
-#     "server_type": "sglang",
-#     "server_gpus": 8,
-# }
+JUDGE_PIPELINE_ARGS = {
+    "model": "/hf_models/Qwen3-235B-A22B-Instruct-2507",
+    "server_type": "sglang",
+    "server_gpus": 8,
+}
