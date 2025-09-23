9e8a357a656d21a2e573f98dc2447f1d03b55e75
Grigor Nalbandyan <gnalbandyan@nvidia.com>
2025-09-23 05:26:35 -0700

    Add  call for summarize_robustness in robust_eval

--- nemo_skills/pipeline/robust_eval.py
+++ nemo_skills/pipeline/robust_eval.py
@@ -17,6 +17,7 @@ import glob
 
 import typer
 
+import nemo_skills.pipeline.utils as pipeline_utils
 from nemo_skills.pipeline.app import app
 from nemo_skills.pipeline.eval import eval
 from nemo_skills.utils import get_logger_name
@@ -28,19 +29,55 @@ LOG = logging.getLogger(get_logger_name(__file__))
 def robust_eval(ctx: typer.Context,
                 prompts_folder: str = typer.Option(..., help="Path to the folder inside /prompt/config/ containing the prompt yaml files to run."),
                 **ns_eval_kwargs):
-    prompts_dir = f"nemo_skills/prompt/config/{prompts_folder}"
-    prompts_to_run = glob.glob(f"{prompts_dir}/*.yaml")
-    if not prompts_to_run:
-        raise ValueError(f"No prompt .yaml files found in the specified directory: {prompts_dir}")
-    LOG.info(f"Found {len(prompts_to_run)} prompts to run in {prompts_dir}")
-    main_output_dir = Path(ns_eval_kwargs['output_dir'])
-    benchmark = ns_eval_kwargs['benchmarks'].split(':')[0] # check that there is only one benchmark
-    for prompt in prompts_to_run:
-        LOG.info(f"Running prompt: {prompt}")
-        ctx.args = [arg for arg in ctx.args if "++prompt_config" not in arg]
-        ctx.args.append(f"++prompt_config={prompt}")
-        ns_eval_kwargs['output_dir'] = str(main_output_dir / benchmark / Path(prompt).stem[:-5])
-        eval(ctx=ctx, **ns_eval_kwargs)
+
+    benchmark_prompts = prompts_folder.split(',')
+    benchmarks = ns_eval_kwargs['benchmarks'].split(',')
+    if len(benchmark_prompts) != len(benchmarks):
+        raise ValueError(f"Number of benchmark names ({len(benchmarks)}) must match number of benchmark prompt folders ({len(benchmark_prompts)})")
+    main_output_dir = ns_eval_kwargs['output_dir']
+    main_expname = ns_eval_kwargs['expname']
+    for benchmark, prompt_folder in zip(benchmarks, benchmark_prompts):
+        prompts_dir = f"nemo_skills/prompt/config/{prompt_folder}"
+        prompts_to_run = glob.glob(f"{prompts_dir}/*.yaml")
+        if not prompts_to_run:
+            raise ValueError(f"No prompt .yaml files found in the specified directory: {prompts_dir}")
+        LOG.info(f"Found {len(prompts_to_run)} prompts to run in {prompts_dir}")
+        dependent_tasks = []
+        ns_eval_kwargs['benchmarks'] = benchmark
+        benchmark = benchmark.split(':')[0]  # Remove any :N suffix for output dir naming
+        for prompt in prompts_to_run:
+            LOG.info(f"Running prompt: {prompt}")
+            ctx.args = [arg for arg in ctx.args if "++prompt_config" not in arg]
+            ctx.args.append(f"++prompt_config={prompt}")
+            ns_eval_kwargs['output_dir'] = f"{main_output_dir}/{benchmark}/{Path(prompt).stem}"
+            ns_eval_kwargs['expname'] = main_expname + f"_{benchmark}_{Path(prompt).stem}"
+            eval(ctx=ctx, **ns_eval_kwargs)
+            dependent_tasks.append(ns_eval_kwargs['expname'])
+
+    cluster_config = pipeline_utils.get_cluster_config(ns_eval_kwargs['cluster'], ns_eval_kwargs.get('config_dir', None))
+
+    _reuse_exp = ns_eval_kwargs.get('_reuse_exp', None)
+    dry_run = ns_eval_kwargs.get('dry_run', False)
+    with pipeline_utils.get_exp(ns_eval_kwargs['expname'], cluster_config, _reuse_exp) as exp:
+        command = (
+                f"python -m nemo_skills.pipeline.summarize_robustness {main_output_dir}"
+            )
+        score_task = pipeline_utils.add_task(
+            exp,
+            cmd=command,
+            task_name=f"summ_robust",
+            log_dir=f"{main_output_dir}/summarize_robustness",
+            container=cluster_config["containers"]["nemo-skills"],
+            partition='cpu',
+            cluster_config=cluster_config,
+            reuse_code_exp=ns_eval_kwargs.get('reuse_code_exp', None),
+            reuse_code=ns_eval_kwargs.get('reuse_code', True),
+            run_after=dependent_tasks,
+            installation_command=ns_eval_kwargs.get('installation_command', None),
+            skip_hf_home_check=ns_eval_kwargs.get('skip_hf_home_check', False),
+        )
+
+        pipeline_utils.run_exp(exp, cluster_config, dry_run=dry_run)
 
 if __name__ == "__main__":
     typer.main.get_command_name = lambda name: name
