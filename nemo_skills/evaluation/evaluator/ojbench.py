bbc5eed3fe26bf5a96183b1a684db5df6528602a
wasiahmad <wasiahmad@ucla.edu>
2025-09-29 16:49:49 -0700

    fix: Prevent permission error in OJBench judger
    Signed-off-by: wasiahmad <wasiahmad@ucla.edu>

--- nemo_skills/evaluation/evaluator/ojbench.py
+++ nemo_skills/evaluation/evaluator/ojbench.py
@@ -106,8 +106,7 @@ async def eval_ojbench_async(cfg):
                 )
             """)
 
-            cmd = 'C_INCLUDE_PATH="" CPLUS_INCLUDE_PATH=""'
-            cmd += f" python -c {shlex.quote(eval_code)}"
+            cmd = f'env -i PATH="/usr/bin:/bin" python -c {shlex.quote(eval_code)}'
             # cmd = "unset C_INCLUDE_PATH CPLUS_INCLUDE_PATH"
             # cmd += f" && python -c {shlex.quote(eval_code)}"
             output, _ = await sandbox.execute_code(
