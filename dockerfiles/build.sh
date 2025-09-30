6738af8a864fbe6c1759029f8c6011b2eff6323d
Sanyam Kapoor <sanyamk@nvidia.com>
2025-09-30 09:09:34 -0700

    only generate name when DOCKER_NAME does not exist
    Signed-off-by: Sanyam Kapoor <sanyamk@nvidia.com>

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -19,21 +19,20 @@ if [[ -z "${1}" ]]; then
     exit 1
 fi
 
-__src_dir="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
-
 __dockerfile="${1}"
 
 __context_dir=$(git rev-parse --show-toplevel 2>/dev/null)
 
-__repo_user=$(basename $(dirname $(git remote get-url origin | sed -E -e "s|[@:]|/|g")))
-__repo_name=$(basename -s .git $(git remote get-url origin))
-__project_name=$(basename "${__dockerfile}")
-if [[ "${__project_name}" == *.* ]]; then
-    __project_name=/$(echo "${__project_name}" | cut -d. -f2)
-else
-    unset __project_name
+if [[ -z "${DOCKER_NAME}" ]]; then
+    __repo_user=$(basename $(dirname $(git remote get-url origin | sed -E -e "s|[@:]|/|g")))
+    __repo_name=$(basename -s .git $(git remote get-url origin))
+    __project_name=$(basename "${__dockerfile}")
+    if [[ "${__project_name}" == *.* ]]; then
+        __project_name=/$(echo "${__project_name}" | cut -d. -f2)
+    else
+        unset __project_name
+    fi
 fi
-
 __docker_name=$(echo ${DOCKER_NAME:-"gitlab-master.nvidia.com/${__repo_user}/${__repo_name}${__project_name}"} | tr "[:upper:]" "[:lower:]")
 __docker_tag=${DOCKER_TAG:-"$(date +"%Y.%m.%d")-$(git rev-parse --short HEAD)"}
 
@@ -46,7 +45,7 @@ if [[ ! -z ${DOCKER_CACHE} ]]; then
     __docker_build_args="${__docker_build_args} --cache-to type=registry,ref=${__docker_name}/cache,mode=max --cache-from type=registry,ref=${__docker_name}/cache"
 fi
 
-docker build ${__docker_build_args} \
-    -f "${__dockerfile}" \
-    -t "${__docker_name}:${__docker_tag}" \
-    "${__context_dir}"
+# docker build ${__docker_build_args} \
+#     -f "${__dockerfile}" \
+#     -t "${__docker_name}:${__docker_tag}" \
+#     "${__context_dir}"
