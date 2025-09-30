f35a37548a9b5c823a531683b25fd3a04318411c
Sanyam Kapoor <sanyamk@nvidia.com>
2025-09-30 15:41:49 -0700

    handle building any dockerfile for more general use, error handling when not git repo
    Signed-off-by: Sanyam Kapoor <sanyamk@nvidia.com>

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ##
-# Build and Push images to Gitlab container registry.
+# Build and Push images.
 #
 # Usage:
 #   ./dockerfiles/build.sh [/path/to/Dockerfile]
@@ -21,31 +21,66 @@ fi
 
 __dockerfile="${1}"
 
-__context_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+##
+#  Conditions to set the context for docker build.
+#   1. If the Dockerfile is part of a git repo, the set to repo root.
+#   2. If not part of git repo, then set to the directory of the Dockerfile.
+#
+__context_dir="$(dirname "$(realpath "${__dockerfile}")")"
+
+pushd "${__context_dir}" > /dev/null
+
+__git_repo_root=$(git rev-parse --show-toplevel 2>/dev/null)
+__is_git_repo=$([[ $? -eq 0 ]] && echo 1 || echo 0)
+
+## If not a git repo, we go back to working directory.
+if [[ $__is_git_repo -eq 0 ]]; then
+    popd > /dev/null
+fi
 
 if [[ -z "${DOCKER_NAME}" ]]; then
-    __repo_user=$(basename $(dirname $(git remote get-url origin | sed -E -e "s|[@:]|/|g")))
-    __repo_name=$(basename -s .git $(git remote get-url origin))
+    __git_remote=$(git remote get-url origin 2>/dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "Dockerfile is not part of a git repo. Set DOCKER_NAME explicitly."
+        exit 1
+    fi
+
+    __repo_user=$(basename $(dirname $(echo ${__git_remote} | sed -E -e "s|[@:]|/|g")))
+    __repo_name=$(basename -s .git $(echo ${__git_remote}))
     __project_name=$(basename "${__dockerfile}")
     if [[ "${__project_name}" == *.* ]]; then
         __project_name=/$(echo "${__project_name}" | cut -d. -f2)
     else
         unset __project_name
     fi
+    DOCKER_NAME="gitlab-master.nvidia.com/${__repo_user}/${__repo_name}${__project_name}"
 fi
-__docker_name=$(echo ${DOCKER_NAME:-"gitlab-master.nvidia.com/${__repo_user}/${__repo_name}${__project_name}"} | tr "[:upper:]" "[:lower:]")
-__docker_tag=${DOCKER_TAG:-"$(date +"%Y.%m.%d")-$(git rev-parse --short HEAD)"}
+DOCKER_NAME=$(echo "${DOCKER_NAME}" | tr "[:upper:]" "[:lower:]")
 
-echo "Building ${__docker_name}:${__docker_tag} from context ${__context_dir}"
+if [[ -z "${DOCKER_TAG}" ]]; then
+    __git_sha=$(git rev-parse --short HEAD 2>/dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "Dockerfile is not part of a git repo. Set DOCKER_TAG explicitly."
+        exit 1
+    fi
+    DOCKER_TAG="$(date +"%Y.%m.%d")-${__git_sha}"
+fi
+
+if [[ ${__is_git_repo} -eq 1 ]]; then
+    __context_dir="${__git_repo_root}"
+    popd > /dev/null
+fi
+
+echo "Building ${DOCKER_NAME}:${DOCKER_TAG} from context ${__context_dir}"
 
 if [[ ! -z ${DOCKER_PUSH} ]]; then
     __docker_build_args="${__docker_build_args} --push"
 fi
 if [[ ! -z ${DOCKER_CACHE} ]]; then
-    __docker_build_args="${__docker_build_args} --cache-to type=registry,ref=${__docker_name}/cache,mode=max --cache-from type=registry,ref=${__docker_name}/cache"
+    __docker_build_args="${__docker_build_args} --cache-to type=registry,ref=${DOCKER_NAME}/cache,mode=max --cache-from type=registry,ref=${DOCKER_NAME}/cache"
 fi
 
 docker build ${__docker_build_args} \
     -f "${__dockerfile}" \
-    -t "${__docker_name}:${__docker_tag}" \
+    -t "${DOCKER_NAME}:${DOCKER_TAG}" \
     "${__context_dir}"
