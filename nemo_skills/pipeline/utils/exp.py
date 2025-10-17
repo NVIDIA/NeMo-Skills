6a477a01257d6466b38ff484f1d18fe3921017df
Vladimir Bataev <vbataev@nvidia.com>
2025-10-18 01:09:41 +0400

    Avoid mpirun for Lepton.
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- nemo_skills/pipeline/utils/exp.py
+++ nemo_skills/pipeline/utils/exp.py
@@ -520,7 +520,7 @@ def add_task(
             total_het_groups=total_het_groups,
             with_ray=with_ray,
         )
-        if cluster_config["executor"] != "slurm" and num_server_tasks > 1:
+        if cluster_config["executor"] not in {"slurm", "lepton"} and num_server_tasks > 1:
             server_cmd = f"mpirun --allow-run-as-root -np {num_server_tasks} bash -c {shlex.quote(server_cmd)}"
         commands.append(server_cmd)
         executors.append(server_executor)
@@ -539,7 +539,7 @@ def add_task(
         if len(cmd) != len(container) or len(cmd) != len(num_tasks):
             raise ValueError("Number of commands, containers and num_tasks must match.")
         for cur_idx, (cur_cmd, cur_container, cur_tasks) in enumerate(zip(cmd, container, num_tasks)):
-            if cluster_config["executor"] != "slurm" and cur_tasks > 1:
+            if cluster_config["executor"] not in {"slurm", "lepton"} and cur_tasks > 1:
                 cur_cmd = f"mpirun --allow-run-as-root -np {cur_tasks} bash -c {shlex.quote(cur_cmd)}"
             with temporary_env_update(cluster_config, {"NEMO_SKILLS_SANDBOX_PORT": sandbox_port}):
                 cur_cmd = install_packages_wrap(cur_cmd, installation_command)
