a451bcc966893b1186ab8e8974fb5c4c3a9f6373
Igor Gitman <igitman@nvidia.com>
2025-10-18 15:41:44 -0700

    Fix bash command
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,7 +39,7 @@ def docker_run(command, image_name=None, volume_paths=None):
             volumes[os.path.abspath(src)] = {"bind": dst, "mode": "rw"}
 
         # Run the container
-        full_command = f"/bin/bash -c '{command}'"
+        full_command = f"bash -c '{command}'"
         result = client.containers.run(
             image_name,
             command=full_command,
