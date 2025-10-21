616ae32703fd08f28141b2d01af587d989e2c467
Shubham Toshniwal <stoshniwal@nvidia.com>
2025-10-21 16:29:40 -0700

    Reverting to original name
    Signed-off-by: Shubham Toshniwal <stoshniwal@nvidia.com>

--- tests/slurm-tests/qwen3_4b_evals/run_test.py
+++ tests/slurm-tests/qwen3_4b_evals/run_test.py
@@ -168,7 +168,7 @@ def main():
     )
 
     # schedule a dependent check job on the cluster and check if the results are as expected
-    checker_cmd = f"python tests/slurm-tests/qwen3_evals/check_results.py --workspace {args.workspace} "
+    checker_cmd = f"python tests/slurm-tests/qwen3_4b_evals/check_results.py --workspace {args.workspace} "
 
     run_cmd(
         ctx=wrap_arguments(checker_cmd),
