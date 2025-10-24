1f5177fa5718e5dab75ba5a4fb243d19d102699c
Igor Gitman <igitman@nvidia.com>
2025-10-23 19:33:47 -0700

    Remove test
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/gpu-tests/run_qwen.sh
+++ tests/gpu-tests/run_qwen.sh
@@ -7,7 +7,7 @@ export NEMO_SKILLS_TEST_MODEL_TYPE=qwen
 # Switch to Qwen3 model for other tests
 export NEMO_SKILLS_TEST_HF_MODEL=Qwen/Qwen3-1.7B
 # generation/evaluation tests
-pytest tests/gpu-tests/test_eval.py -s -x -k test_prepare_and_eval_all_datasets
+pytest tests/gpu-tests/test_eval.py -s -x
 pytest tests/gpu-tests/test_generate.py -s -x
 pytest tests/gpu-tests/test_judge.py -s -x
 pytest tests/gpu-tests/test_run_cmd_llm_infer.py -s -x
