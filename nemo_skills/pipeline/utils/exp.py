0849ab0b70758e5f4b7bc5aec4c793745c412cff
Alex Gronskiy <agronskiy@nvidia.com>
2025-10-28 19:27:13 +0100

    chore: asserts
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>

--- nemo_skills/pipeline/utils/exp.py
+++ nemo_skills/pipeline/utils/exp.py
@@ -255,7 +255,7 @@ def get_executor(
             additional_kwargs["runtime"] = "runc"
         # wipp
         print(f"WIPP num_gpus_per_node {gpus_per_node}")
-        assert gpus_per_node is None
+        assert gpus_per_node is None  # WIPP
         d = DockerExecutor(
             container_image=resolved_container,
             packager=packager,
