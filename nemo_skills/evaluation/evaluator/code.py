c62cb54560bfb9c5ce880da88ba2452f99c05df8
Aleksander Ficek <alex.ficek99@gmail.com>
2025-11-02 07:36:20 -0800

    Add changes
    Signed-off-by: Aleks Ficek <alex.ficek99@gmail.com>

--- nemo_skills/evaluation/evaluator/code.py
+++ nemo_skills/evaluation/evaluator/code.py
@@ -51,10 +51,10 @@ class CodeExecEvaluator(BaseEvaluator):
         super().__init__(config, num_parallel_requests)
         self.eval_config = CodeExecEvaluatorConfig(**self.config)
         self.sandbox = get_sandbox(self.eval_config.sandbox)
-        # self.sandbox.wait_for_sandbox(50)
-        # import time
+        self.sandbox.wait_for_sandbox(50)
+        import time
 
-        # time.sleep(10)
+        time.sleep(10)
 
     async def eval_single(self, data: dict):
         """Evaluate single code during generation."""
