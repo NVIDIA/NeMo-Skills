e1e18f4caaad0992380eb1c965d5f046952bceb5
Alex Gronskiy <agronskiy@nvidia.com>
2025-11-07 00:11:32 +0100

    chore: cleanup metrics
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>

--- nemo_skills/evaluation/metrics/map_metrics.py
+++ nemo_skills/evaluation/metrics/map_metrics.py
@@ -35,7 +35,6 @@ from nemo_skills.evaluation.metrics.lean4_metrics import Lean4Metrics
 from nemo_skills.evaluation.metrics.math_metrics import MathMetrics
 from nemo_skills.evaluation.metrics.mmau_pro_metrics import MMAUProMetrics
 from nemo_skills.evaluation.metrics.mrcr_metrics import MRCRMetrics
-from nemo_skills.evaluation.metrics.nemo_evaluator_metrics import NemoEvaluatorMetrics
 from nemo_skills.evaluation.metrics.ruler_metrics import RulerMetrics
 from nemo_skills.evaluation.metrics.simpleqa_metrics import SimpleQAMetrics
 from nemo_skills.evaluation.metrics.translation_metrics import TranslationMetrics
